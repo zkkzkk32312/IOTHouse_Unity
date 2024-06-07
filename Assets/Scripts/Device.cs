1ad1cea2aa17058d43728ec2647fc9c26d3f50a9
Zack <zkkzkk32312@msn.com>
2024-06-07 10:57:21 -0600

    Add cinemachine/dotween/odin/outline,
    Add camera & outline logic in DeviceManager.

--- Assets/Scripts/Device.cs
+++ Assets/Scripts/Device.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using Sirenix.OdinInspector;
 
 [Serializable]
 public class Device : MonoBehaviour
@@ -11,10 +13,52 @@ public class Device : MonoBehaviour
     public string Type;
     [SerializeField]
     public List<string> TelemetryNames;
+    [SerializeField]
+    public bool UseCustomMeshes;
+    [SerializeField]
+    public List<GameObject> Meshes;
+
+    List<Outline> Outlines = new List<Outline>();
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        if (!UseCustomMeshes)
+        {
+            var outline = GetComponent<Outline>();
+            Outlines.Add(outline);
+        }
+        else
+        {
+            foreach(GameObject go in Meshes)
+            {
+                var outline = go.GetComponent<Outline>();
+                Outlines.Add(outline);
+            }
+        }
+    }
+
+    [Button]
+    public void StartOutline()
+    {
+        foreach (Outline ol in Outlines)
+        {
+            ol.enabled = true;
+            ol.OutlineWidth = 0;
+            DOTween.To(() => ol.OutlineWidth, x =>
+            {
+                ol.OutlineWidth = x;
+            }, 6, .7f).SetLoops(-1, LoopType.Yoyo);
+        }
+    }
+
+    [Button]
+    public void StopOutline()
+    {
+        foreach (Outline ol in Outlines)
+        {
+            ol.enabled = false;
+            ol.OutlineWidth = 0;
+        }
     }
 }
--- Assets/Scripts/DeviceManager.cs
+++ Assets/Scripts/DeviceManager.cs
@@ -1,10 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Cinemachine;
+using DG.Tweening;
+using Sirenix.OdinInspector;
 
 public class DeviceManager : MonoBehaviour
 {
     public static DeviceManager Instance { get; private set; }
     public List<Device> devices;
+    CinemachineCamera cm;
+    Device lastDevice = null;
 
     private void Awake()
     {
@@ -21,11 +26,26 @@ public class DeviceManager : MonoBehaviour
 
     void Start()
     {
-        
+        cm = FindFirstObjectByType<CinemachineCamera>();
     }
 
     void Update()
     {
         
+    }
+
+    [Button]
+    public void SelectDevice (int index)
+    {
+        if (lastDevice != null)
+        {
+            lastDevice.StopOutline();
+            lastDevice = null;
+        }
+
+        Device device = devices[index];
+        cm.Target.TrackingTarget.transform.DOMove(device.transform.position, 2f);
+        device.StartOutline();
+        lastDevice = device;
     }
 }
